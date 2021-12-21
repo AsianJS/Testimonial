d7f9a5e52defafcf20f07837a517605d80ed47b1
AsianJS <92735221+AsianJS@users.noreply.github.com>
2021-12-21 09:12:21 -0500

    Added Quotes
    Quotes has been added to the website.

--- JS/script.js
+++ JS/script.js
@@ -8,4 +8,5 @@ var quotesArray = [
   'NAPS is doing the hard work of helping people get the rest they need. — U.S. News',
 ];
 var randomNumber = Math.floor(Math.random() * quotesArray.length);
-document.getElementById('press-quote').textContent = quotesArray[randomNumber];
+document.getElementById('press-quote').textContent = '“' + quotesArray[randomNumber].split(' — ')[0] + '” — ' + quotesArray[randomNumber].split(' — ')[1];
+console.log(quotesArray[randomNumber].split(' — ')[0]);
